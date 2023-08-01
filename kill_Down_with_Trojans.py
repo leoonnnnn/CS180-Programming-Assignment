ad954649431b43b101e8688376574efff1e7f786
leoonnnnn <39069236+leoonnnnn@users.noreply.github.com>
2023-08-01 13:57:23 -0700

    added prot tokens

--- kill_Down_with_Trojans.py
+++ kill_Down_with_Trojans.py
@@ -26,24 +26,26 @@ def print_tile_data(tile_types, tile_values):
 
 
 def DP(n, H, tile_types, tile_values):
-    memo = np.empty((n, n))
+    memo = np.empty((n, n, 2))
     memo[:] = np.nan                 # use np.nan as the null value
 
     print("\nmemo before:")      # COMMENT OUT!!!
     print(memo)                  # COMMENT OUT!!!
-    res = H + DP_helper(memo, n, H, tile_types, tile_values, 0, 0)
+    res = H + DP_helper(memo, n, H, tile_types, tile_values, 0, 0, 0)
     print("memo after:")         # COMMENT OUT!!!
     print(memo)                  # COMMENT OUT!!!
     print("Final hp: ", res)     # COMMENT OUT!!!
     return res >= 0
 
 
-def DP_helper(memo, n, hp, tile_types, tile_values, x, y):  #add tokens later
-    #BCs
+def DP_helper(memo, n, hp, tile_types, tile_values, x, y, pTok):  #add tokens later
+    #BCs -------------------------
     if x >= n or y >= n:    #out of bounds
         return -100000
     if hp < 0:
-        return -123    # can't revive, so penalize reviving    #change to -100000
+        return -123    # not allowed to revive, so penalize reviving    #change to -100000
+    if not np.isnan(memo[x][y][pTok]):
+        return memo[x][y][pTok]
 
     type = 0
     if tile_types[x][y] == 0:
@@ -52,21 +54,32 @@ def DP_helper(memo, n, hp, tile_types, tile_values, x, y):  #add tokens later
     elif tile_types[x][y] == 1:
         type = 1         #heal
         #print(x, y, "heal")
+    elif tile_types[x][y] == 2:
+        pTok = 1         # pick up protection token
+        print("picked up prot token, count: ", pTok)
     #else:
         #maybe for token shit
+    pTemp = 1
+    if tile_types[x][y] == 0 and pTok == 1:
+        pTok = 0        # use protection token
+        pTemp = 0
+        print("used prot token, count: ", pTok)
 
     if x == n-1 and y == n-1:
-        return tile_values[x][y] * type   # reached end
-    if not np.isnan(memo[x][y]):
-        return memo[x][y]
+        return tile_values[x][y] * type * pTemp   # reached bottom right
+    #BCs end ---------------------
 
+    #print(x, y, pTok, memo[x][y][pTok])
     #print(x, y, type, tile_values[x][y])
-    #print(tile_types[x][y])    #wait this is actually useful lol, literally tells you the tile type
+    #print(tile_types[x][y])
     curval = tile_values[x][y] * type
-    hp += curval
-    opt1 = DP_helper(memo, n, hp, tile_types, tile_values, x+1, y) + curval   # move down, lowkey just call it down and right instead of opt1 and opt2... (tho in future opts can also include using tokens :shrug:)
-    opt2 = DP_helper(memo, n, hp, tile_types, tile_values, x, y+1) + curval    # move right
-    memo[x][y] = max(opt1, opt2) 
+    hp += curval     
+    #if it doesnt affect the memo below, more elegant code would be ... + curval * pTok. and then just set pTok to 0 beforehand if gonna use it
+    # see and then in the next call, pTok will have the right value (esp if used)
+    opt1 = DP_helper(memo, n, hp, tile_types, tile_values, x+1, y, pTok) + curval * pTemp    # move down
+    opt2 = DP_helper(memo, n, hp, tile_types, tile_values, x, y+1, pTok) + curval * pTemp    # move right
+    memo[x][y][pTok] = max(opt1, opt2)    #should it be pTok or use_pTok (like this or inverse, think about it with some simple examples)
+    #print(pTok, pTemp)
     return max(opt1, opt2)   #test that it works by spitting out the max path sum
 
 
